9cfa6a4609d46774b4934721f3647d3fa5aaad8f
yigudove <yigu32@outlook.com>
2023-08-07 02:48:34 +0800

    create DemoEnemy and DemoBulletHitInterface Class

---
+++ Source/ygd_tps_demo/Private/DemoBulletHitInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DemoBulletHitInterface.h"
+
+// Add default functionality here for any IDemoBulletHitInterface functions that are not pure virtual.
