66ea7bd44616c07d33e27a88e3f2cd5dc1d2e7fc
yigudove <yigu32@outlook.com>
2023-08-08 05:06:44 +0800

    init DefaultEnemy and its AnimInstance

---
+++ Source/ygd_tps_demo/Public/DemoEnemyAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "DemoEnemyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YGD_TPS_DEMO_API UDemoEnemyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
