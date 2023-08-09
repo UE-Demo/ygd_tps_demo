05d1d25ad29bc55187e3e781b684223b0f547ebb
yigudove <yigu32@outlook.com>
2023-08-10 04:13:49 +0800

    Set up DefaultEnemy Anim
    switch between idle and jog

--- Source/ygd_tps_demo/Private/DemoEnemyAnimInstance.cpp
+++ Source/ygd_tps_demo/Private/DemoEnemyAnimInstance.cpp
@@ -3,3 +3,18 @@
 
 #include "DemoEnemyAnimInstance.h"
 
+void UDemoEnemyAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (Enemy == nullptr)
+	{
+		Enemy = Cast<ADemoEnemy>(TryGetPawnOwner());
+	}
+
+	if (Enemy)
+	{
+		FVector EnemyVelocity{ Enemy->GetVelocity() };
+		EnemyVelocity.Z = 0.f;
+		EnemySpeed = EnemyVelocity.Size();
+	}
+	else { UE_LOG(LogTemp, Warning, TEXT("No EnemyBP binding to EnemyAnimInstance")); }
+}
--- Source/ygd_tps_demo/Public/DemoEnemyAnimInstance.h
+++ Source/ygd_tps_demo/Public/DemoEnemyAnimInstance.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DemoEnemy.h"
+
 #include "Animation/AnimInstance.h"
 #include "DemoEnemyAnimInstance.generated.h"
 
@@ -14,4 +16,15 @@ class YGD_TPS_DEMO_API UDemoEnemyAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+
+protected:
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	ADemoEnemy* Enemy;
+
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	float EnemySpeed;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
 };
